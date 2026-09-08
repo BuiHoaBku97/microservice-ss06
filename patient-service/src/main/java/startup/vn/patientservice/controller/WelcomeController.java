582e1603a1b36d4973c587404c93f92659b3dac9
BuiHoaBku97 <vanhoabku97@gmail.com>
2026-09-08 23:17:33 +0700

    - RefreshScope

---
+++ patient-service/src/main/java/startup/vn/patientservice/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package startup.vn.patientservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+@RequestMapping("/welcome")
+public class WelcomeController {
+    @Value("${app.welcome:Welcome to patient service}")
+    private String welcome;
+
+    @GetMapping
+    public String welcome() {
+        return welcome;
+    }
+}
